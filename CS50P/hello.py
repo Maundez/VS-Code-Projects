d5ffa7a36d59fb21ce25e696a32b4a73c1d72657
Maundez <smaunder@gmail.com>
2023-10-14 10:32:51 +1100

    A few changes

--- CS50P/hello.py
+++ CS50P/hello.py
@@ -2,10 +2,12 @@
 def main():
     name = input("What's your name? ").title().strip()
     hello(name)
+    
+    
 
 #say hello to the user
-def hello(to='world'):
-    return f"hello, {to}"
+def hello(to = "world"):
+   print(f"hello, {to}")
 
 if __name__ == "__main__":
     main()
